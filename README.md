4d77221bb30d55f082533204336496b27b9d1f5c
AnupShrestha28 <anupstha0168@gmail.com>
2023-07-09 01:11:59 +0545

    Photogram app completed

---
+++ README.md
@@ -0,0 +1,2 @@
+# Photogram-React
+These app allows users to upload photos which will store in firebase and display them in a grid view.
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+
+const Title = () => {
+  return (
+    <div className='title'>
+        <h1>PhotoGram</h1>
+        <h2>Uploaded pictures display in a grid view</h2>
+        <p>Upload Pictures</p>
+    </div>
+  )
+}
+
+export default Title
